48f2b56dd73c78a506373bebb7b38d7f205f7b03
Elioenai.Gonçalves <elioenai.andrade@hotmail.com>
2024-09-12 23:41:21 -0300

    adicionando projeto demo

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoRazorPages.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+public DbSet<Aluno> Aluno { get; set; } = default!;
+}
---
+++ Pages/Alunos/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DemoRazorPages.Data;
+
+namespace DemoRazorPages.Pages_Alunos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DemoRazorPages.Data.ApplicationDbContext _context;
+
+        public IndexModel(DemoRazorPages.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Aluno> Aluno { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Aluno = await _context.Aluno.ToListAsync();
+        }
+    }
+}
